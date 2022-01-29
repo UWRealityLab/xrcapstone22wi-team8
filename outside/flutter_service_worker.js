b38183f62906c8944cb3b45ff6f31c9aff9e1a7c
ApolloZhu <ApolloZhu@users.noreply.github.com>
2022-01-29 07:30:53 +0000

    deploy: 03d2f282c06a28d7628bb5a84455b73e6c23c945

--- outside/flutter_service_worker.js
+++ outside/flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "e22ff1e8ffa488ead531a3e32c68c160",
-"/": "e22ff1e8ffa488ead531a3e32c68c160",
+  "index.html": "c07873a6670f5ae914a726e09a1b2866",
+"/": "c07873a6670f5ae914a726e09a1b2866",
 "version.json": "e58fa0c28a36e08cf181b0f92af6090b",
 "manifest.json": "1b8d92e038d2412952b939865038bc80",
 "main.dart.js": "9dd7f9782d5882254c98f14a42a5c3f8",
