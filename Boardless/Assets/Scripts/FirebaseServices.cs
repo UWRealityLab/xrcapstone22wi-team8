646030fa9c8596a8138f9c398611befb3edf0207
Apollo Zhu <zhuzhiyu@cs.washington.edu>
2022-02-07 02:15:50 -0800

    Eureka! The boat floats!
    Why was this so complicated??? And why am I committing so many files???

---
+++ Boardless/Assets/Scripts/FirebaseServices.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using UnityEngine;
+
+public class FirebaseServices : MonoBehaviour
+{
+    private FirebaseApp _app = null;
+    public GameObject Success;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                _app = Firebase.FirebaseApp.DefaultInstance;
+                UnityEngine.Debug.Log("Firebase initialized");
+                Instantiate(Success);
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+}
