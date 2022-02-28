eb36c71ce25bd15404784648f7bbffcf286a4fe8
KH-Kyle <kyle.gzhang@gmail.com>
2022-02-28 11:50:46 -0800

    initial push

---
+++ Boardless/Assets/Scripts/TesseractDemoScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TesseractDemoScript : MonoBehaviour
+{
+    [SerializeField] private Texture2D imageToRecognize;
+    [SerializeField] private Text displayText;
+    [SerializeField] private RawImage outputImage;
+    private TesseractDriver _tesseractDriver;
+    private string _text = "";
+    private Texture2D _texture;
+
+    private void Start()
+    {
+        Texture2D texture = new Texture2D(imageToRecognize.width, imageToRecognize.height, TextureFormat.ARGB32, false);
+        texture.SetPixels32(imageToRecognize.GetPixels32());
+        texture.Apply();
+
+        _tesseractDriver = new TesseractDriver();
+        Recoginze(texture);
+    }
+
+    private void Recoginze(Texture2D outputTexture)
+    {
+        _texture = outputTexture;
+        ClearTextDisplay();
+        AddToTextDisplay(_tesseractDriver.CheckTessVersion());
+        _tesseractDriver.Setup(OnSetupCompleteRecognize);
+    }
+
+    private void OnSetupCompleteRecognize()
+    {
+        AddToTextDisplay(_tesseractDriver.Recognize(_texture));
+        AddToTextDisplay(_tesseractDriver.GetErrorMessage(), true);
+        SetImageDisplay();
+    }
+
+    private void ClearTextDisplay()
+    {
+        _text = "";
+    }
+
+    private void AddToTextDisplay(string text, bool isError = false)
+    {
+        if (string.IsNullOrWhiteSpace(text)) return;
+
+        _text += (string.IsNullOrWhiteSpace(displayText.text) ? "" : "\n") + text;
+
+        if (isError)
+            Debug.LogError(text);
+        else
+            Debug.Log(text);
+    }
+
+    private void LateUpdate()
+    {
+        displayText.text = _text;
+    }
+
+    private void SetImageDisplay()
+    {
+        RectTransform rectTransform = outputImage.GetComponent<RectTransform>();
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
+            rectTransform.rect.width * _tesseractDriver.GetHighlightedTexture().height / _tesseractDriver.GetHighlightedTexture().width);
+        outputImage.texture = _tesseractDriver.GetHighlightedTexture();
+    }
+}
---
+++ Boardless/Assets/Scripts/TesseractDriver.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TesseractDriver
+{
+    private TesseractWrapper _tesseract;
+    private static readonly List<string> fileNames = new List<string> {"tessdata.tgz"};
+
+    public string CheckTessVersion()
+    {
+        _tesseract = new TesseractWrapper();
+
+        try
+        {
+            string version = "Tesseract version: " + _tesseract.Version();
+            Debug.Log(version);
+            return version;
+        }
+        catch (Exception e)
+        {
+            string errorMessage = e.GetType() + " - " + e.Message;
+            Debug.LogError("Tesseract version: " + errorMessage);
+            return errorMessage;
+        }
+    }
+
+    public void Setup(UnityAction onSetupComplete)
+    {
+#if UNITY_EDITOR
+        OcrSetup(onSetupComplete);
+#elif UNITY_ANDROID
+        CopyAllFilesToPersistentData(fileNames, onSetupComplete);
+#else
+        OcrSetup(onSetupComplete);
+#endif
+    }
+
+    public void OcrSetup(UnityAction onSetupComplete)
+    {
+        _tesseract = new TesseractWrapper();
+
+#if UNITY_EDITOR
+        string datapath = Path.Combine(Application.streamingAssetsPath, "tessdata");
+#elif UNITY_ANDROID
+        string datapath = Application.persistentDataPath + "/tessdata/";
+#else
+        string datapath = Path.Combine(Application.streamingAssetsPath, "tessdata");
+#endif
+
+        if (_tesseract.Init("eng", datapath))
+        {
+            Debug.Log("Init Successful");
+            onSetupComplete?.Invoke();
+        }
+        else
+        {
+            Debug.LogError(_tesseract.GetErrorMessage());
+        }
+    }
+
+    private async void CopyAllFilesToPersistentData(List<string> fileNames, UnityAction onSetupComplete)
+    {
+        String fromPath = "jar:file://" + Application.dataPath + "!/assets/";
+        String toPath = Application.persistentDataPath + "/";
+
+        foreach (String fileName in fileNames)
+        {
+            if (!File.Exists(toPath + fileName))
+            {
+                Debug.Log("Copying from " + fromPath + fileName + " to " + toPath);
+                WWW www = new WWW(fromPath + fileName);
+
+                while (!www.isDone)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(Time.deltaTime));
+                }
+
+                File.WriteAllBytes(toPath + fileName, www.bytes);
+                Debug.Log("File copy done");
+                www.Dispose();
+                www = null;
+            }
+            else
+            {
+                Debug.Log("File exists! " + toPath + fileName);
+            }
+
+            UnZipData(fileName);
+        }
+
+        OcrSetup(onSetupComplete);
+    }
+
+    public string GetErrorMessage()
+    {
+        return _tesseract?.GetErrorMessage();
+    }
+
+    public string Recognize(Texture2D imageToRecognize)
+    {
+        return _tesseract.Recognize(imageToRecognize);
+    }
+
+    public Texture2D GetHighlightedTexture()
+    {
+        return _tesseract.GetHighlightedTexture();
+    }
+
+    private void UnZipData(string fileName)
+    {
+        if (File.Exists(Application.persistentDataPath + "/" + fileName))
+        {
+            UnZipUtil.ExtractTGZ(Application.persistentDataPath + "/" + fileName, Application.persistentDataPath);
+            Debug.Log("UnZipping Done");
+        }
+        else
+        {
+            Debug.LogError(fileName + " not found!");
+        }
+    }
+}
