205ecdadce54e548e30dcecdf143d074b9135dab
Apollo Zhu <zhuzhiyu@cs.washington.edu>
2022-03-01 11:47:40 -0800

    Support loading obj (and their mtl files)
    No direct loading of mtl since that doesn't make any sense

--- Boardless/Assets/Scripts/FirebaseFilesDropdown.cs
+++ Boardless/Assets/Scripts/FirebaseFilesDropdown.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Firebase.Firestore;
 using Firebase.Storage;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class FirebaseFilesDropdown : MonoBehaviour
 {
@@ -13,8 +16,9 @@ public class FirebaseFilesDropdown : MonoBehaviour
     public GameObject TextContainerPrefab;
 
     // document id -> file
-    private SortedList<string, File> _documents = new SortedList<string, File>();
-    private struct File {
+    private SortedList<string, FirebaseFile> _documents = new SortedList<string, FirebaseFile>();
+    private struct FirebaseFile
+    {
         public string Name;
         public string RefOrNull;
         public string ContentOrNull;
@@ -32,6 +36,11 @@ public class FirebaseFilesDropdown : MonoBehaviour
         {
             OnDropdownValueChange(_dropDown);
         });
+        string directoryPath = LocalDirectory();
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
     }
 
     private void OnRoomDocumentsChange(QuerySnapshot snapshot)
@@ -40,7 +49,7 @@ public class FirebaseFilesDropdown : MonoBehaviour
         {
             string id = change.Document.Id;
             int index;
-            File file;
+            FirebaseFile file;
             switch (change.ChangeType)
             {
                 case DocumentChange.Type.Added:
@@ -48,6 +57,8 @@ public class FirebaseFilesDropdown : MonoBehaviour
                     index = _documents.Keys.IndexOf(id) + 1;
                     file = FileForDocument(change.Document);
                     _dropDown.options.Insert(index, new Dropdown.OptionData { text = file.Name });
+                    // need to aggreesively download for 3D object loading
+                    DownloadIfFileRef(file, false);
                     break;
                 case DocumentChange.Type.Modified:
                     file = FileForDocument(change.Document);
@@ -58,27 +69,75 @@ public class FirebaseFilesDropdown : MonoBehaviour
                         _dropDown.options.RemoveAt(index);
                         _dropDown.options.Insert(index, new Dropdown.OptionData { text = file.Name });
                     }
+                    DownloadIfFileRef(file, true);
                     break;
                 case DocumentChange.Type.Removed:
                     index = _documents.Keys.IndexOf(id) + 1;
                     _dropDown.options.RemoveAt(index);
                     _documents.Remove(id);
+                    // we cannot delete file because it can still be in use
                     break;
             }
         }
     }
 
-    private File FileForDocument(DocumentSnapshot document)
+    private FirebaseFile FileForDocument(DocumentSnapshot document)
     {
         string refOrNull, contentOrNull;
         document.TryGetValue("ref", out refOrNull);
         document.TryGetValue("content", out contentOrNull);
-        return new File
+        return new FirebaseFile
         {
             Name = FirebaseServices.GetDisplayName(document),
             RefOrNull = refOrNull,
             ContentOrNull = contentOrNull,
         };
+    }
+
+    private string LocalDirectory()
+    {
+        return Path.Combine(Application.persistentDataPath, FirebaseServices.Room);
+    }
+
+    private string LocalPathForFile(FirebaseFile file)
+    {
+        if (file.RefOrNull == null)
+        {
+            return null;
+        }
+        else
+        {
+            return Path.Combine(LocalDirectory(), file.RefOrNull);
+        }
+    }
+
+    private void DownloadIfFileRef(FirebaseFile file, bool downloadIfExists)
+    {
+
+        string fullName = LocalPathForFile(file);
+        if (fullName == null)
+        {
+            return;
+        }
+        if (File.Exists(fullName) && !downloadIfExists)
+        {
+            Debug.Log($"File exists at {fullName} and no download required.");
+            return;
+        }
+        Firebase.RefForDownload(file.RefOrNull)
+            // Firebase expects starting with file://
+            .GetFileAsync(new Uri(fullName).AbsoluteUri)
+            .ContinueWith(task =>
+            {
+                if (!task.IsFaulted && !task.IsCanceled)
+                {
+                    Debug.Log($"File download OK: {fullName}.");
+                }
+                else
+                {
+                    Debug.Log($"File download NO: {fullName}.");
+                }
+            });
     }
 
     private void OnDropdownValueChange(Dropdown dropdown)
@@ -90,7 +149,7 @@ public class FirebaseFilesDropdown : MonoBehaviour
             return;
         }
         int fileIndex = index - 1;
-        File file = _documents[_documents.Keys[fileIndex]];
+        FirebaseFile file = _documents[_documents.Keys[fileIndex]];
         if (file.RefOrNull != null)
         {
             InstantiateFile(file);
@@ -105,22 +164,106 @@ public class FirebaseFilesDropdown : MonoBehaviour
 
     private void InstantiateTextDisplay(string text)
     {
+        if (TextContainerPrefab == null)
+        {
+            Debug.LogError("FirebaseFileDropdown No TextContainerPrefab");
+            Debug.Log(text);
+            return;
+        }
         GameObject textContainer = Instantiate(TextContainerPrefab, SpawnLocation, Quaternion.identity);
         textContainer.GetComponentInChildren<TextMesh>().text = text;
     }
 
-    private void InstantiateFile(File file)
+    private void InstantiateFile(FirebaseFile file)
     {
-        // FIXME: implement handling per type.
-        // Please arrange file suffix alphabetically to reduce merge conflicts.
-        StorageReference storageRef = Firebase.RefForDownload(file.RefOrNull);
-
-        if (file.Name.EndsWith("*.png"))
+        string path = LocalPathForFile(file);
+        if (path == null)
         {
-            InstantiateTextDisplay($"Can't display {file.Name}");
+            Debug.LogError($"Trying to instantiate ${file.Name} without file in storage");
             return;
         }
+        if (!File.Exists(path))
+        {
+            Debug.LogWarning($"{file.Name} isn't downloaded to ${path}");
+            InstantiateTextDisplay($"{file.Name} is not available at this time");
+            return;
+        }
+        string extension = Path.GetExtension(path).ToLower();
+        // FIXME: implement handling per type.
+        // Please arrange file extensions alphabetically to reduce merge conflicts.
+        Debug.Log($"Trying to display {file.Name} at {path} with extension {extension}");
+        switch (extension)
+        {
+            case ".obj":
+                GameObject newObject = new Dummiesman.OBJLoader().Load(path);
+                if (newObject == null) break;
+                newObject.transform.position = SpawnLocation;
 
+                Vector3 size = GetObjBounds(newObject).size;
+                Debug.Log(size);
+
+                if (size == Vector3.zero)
+                {
+                    // not working
+                    break;
+                }
+
+                BoxCollider collider = newObject.AddComponent<BoxCollider>();
+                collider.size = size;
+
+                // scale to about 0.5 so not too big not to small
+                // https://stackoverflow.com/a/31670874
+                float minSize = Math.Min(size.x, Math.Min(size.y, size.z));
+                Vector3 newScale = newObject.transform.localScale * (0.5f / minSize);
+                // box collider want positive, fine
+                newScale.x = Math.Abs(newScale.x);
+                newScale.y = Math.Abs(newScale.y);
+                newScale.z = Math.Abs(newScale.z);
+
+                newObject.transform.localScale = newScale;
+                Debug.Log(newScale);
+
+                AddXRInteractions(newObject);
+
+                return;
+            default:
+                break;
+        }
         InstantiateTextDisplay($"Can't display {file.Name}");
+        return;
+    }
+
+    // https://answers.unity.com/questions/208645/true-scale-of-a-visible-gameobject-with-many-child.html
+    private Bounds GetObjBounds(GameObject newObject)
+    {
+        Bounds bounds = new Bounds(newObject.transform.position, Vector3.zero);
+        foreach (Renderer r in newObject.GetComponentsInChildren<Renderer>())
+        {
+            bounds.Encapsulate(r.bounds);
+        }
+        return bounds;
+    }
+
+    private void AddXRInteractions(GameObject newObject)
+    {
+        MoveWithController moveWithController = newObject.AddComponent<MoveWithController>();
+        moveWithController.InputScale = 0.3f;
+        ChangeScale changeScale = newObject.AddComponent<ChangeScale>();
+        changeScale.InputScale = 0.3f;
+
+        XRSimpleInteractable interactable = newObject.AddComponent<XRSimpleInteractable>();
+        interactable.selectMode = InteractableSelectMode.Single;
+        interactable.selectEntered.AddListener((arg0) =>
+        {
+            moveWithController.ActivateMove();
+            changeScale.ActivateScaling();
+        });
+        interactable.selectExited.AddListener((arg0) =>
+        {
+            moveWithController.DeactivateMove();
+            changeScale.DeactivatScaling();
+        });
+
+        // FIXME: implement sync over Photon
     }
 }
