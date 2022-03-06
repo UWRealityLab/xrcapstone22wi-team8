00274f246137bfc2007a93039999eb852a493128
unknown <yueqianz@uw.edu>
2022-03-05 23:19:06 -0800

    fine

--- Boardless/Assets/Scripts/Whiteboard.cs
+++ Boardless/Assets/Scripts/Whiteboard.cs
@@ -17,8 +17,8 @@ public class Whiteboard : MonoBehaviour
     void Start()
     {
         r = GetComponent<Renderer>();
-        textureSize.x = (int)(200*r.transform.localScale.x);
-        textureSize.y = (int)(200*r.transform.localScale.y);
+        textureSize.x = (int)(300*r.transform.localScale.x);
+        textureSize.y = (int)(300*r.transform.localScale.y);
         texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
         r.material.mainTexture = texture;
         Color fillColor = Color.white;
@@ -33,62 +33,64 @@ public class Whiteboard : MonoBehaviour
     }
 
     void Update() {
-        Debug.Log(texture.width);
-        Debug.Log((int)(200*r.transform.localScale.x));
-        if (texture.width != (int)(200*r.transform.localScale.x) || texture.height != (int)(200*r.transform.localScale.y)) {
-            textureSize.x = (int)(200*r.transform.localScale.x);
-            textureSize.y = (int)(200*r.transform.localScale.y);
-            //texture = Resize(texture, (int)(200*r.transform.localScale.x), (int)(200*r.transform.localScale.y));
-            texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
-            r.material.mainTexture = texture;
-            Color fillColor = Color.white;
-            var fillColorArray =  texture.GetPixels();
-            
-            for(var i = 0; i < fillColorArray.Length; ++i)
-            {
-                fillColorArray[i] = fillColor;
+        if (texture.width != (int)(300*r.transform.localScale.x) || texture.height != (int)(300*r.transform.localScale.y)) {
+            textureSize.x = (int)(300*r.transform.localScale.x);
+            textureSize.y = (int)(300*r.transform.localScale.y);
+            if ((int)textureSize.x == 0 || (int)textureSize.y == 0) {
+                return;
             }
-            texture.SetPixels( fillColorArray );
-            texture.Apply();
+            Scale(texture, (int)textureSize.x, (int)textureSize.y);
+            // texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+            // r.material.mainTexture = texture;
+            // Color fillColor = Color.white;
+            // var fillColorArray =  texture.GetPixels();
+            
+            // for(var i = 0; i < fillColorArray.Length; ++i)
+            // {
+            //     fillColorArray[i] = fillColor;
+            // }
+            // texture.SetPixels( fillColorArray );
+            // texture.Apply();
         }
     }
     
-	// public void Scale(Texture2D tex, int newWidth, int newHeight) {
-	// 	texColors = tex.GetPixels();
-	// 	newColors = new Color[newWidth * newHeight];
-	// 	ratioX = 1.0f / ((float)newWidth / (tex.width-1));
-	// 	ratioY = 1.0f / ((float)newHeight / (tex.height-1));
-	// 	w = tex.width;
-	// 	w2 = newWidth;
+	public void Scale(Texture2D tex, int newWidth, int newHeight) {
+        Debug.Log("look at me");
+		texColors = tex.GetPixels();
+		newColors = new Color[newWidth * newHeight];
+		ratioX = 1.0f / ((float)newWidth / (tex.width-1));
+		ratioY = 1.0f / ((float)newHeight / (tex.height-1));
+		w = tex.width;
+		w2 = newWidth;
  
-	// 	BilinearScale(0, newHeight);
+		BilinearScale(0, newHeight);
  
-	// 	tex.Resize(newWidth, newHeight);
-	// 	tex.SetPixels(newColors);
-	// 	tex.Apply();
-	// }
+		tex.Resize(newWidth, newHeight);
+		tex.SetPixels(newColors);
+		tex.Apply();
+	}
  
-	// private void BilinearScale(int start, int end) {
-	// 	for (var y = start; y < end; y++) {
-	// 		int yFloor = (int)Mathf.Floor(y * ratioY);
-	// 		var y1 = yFloor * w;
-	// 		var y2 = (yFloor+1) * w;
-	// 		var yw = y * w2;
+	private void BilinearScale(int start, int end) {
+		for (var y = start; y < end; y++) {
+			int yFloor = (int)Mathf.Floor(y * ratioY);
+			var y1 = yFloor * w;
+			var y2 = (yFloor+1) * w;
+			var yw = y * w2;
  
-	// 		for (var x = 0; x < w2; x++) {
-	// 			int xFloor = (int)Mathf.Floor(x * ratioX);
-	// 			var xLerp = x * ratioX-xFloor;
-	// 			newColors[yw + x] = ColorLerpUnclamped(ColorLerpUnclamped(texColors[y1 + xFloor], texColors[y1 + xFloor+1], xLerp),
-	// 												   ColorLerpUnclamped(texColors[y2 + xFloor], texColors[y2 + xFloor+1], xLerp),
-	// 												   y*ratioY-yFloor);
-	// 		}
-	// 	}
-	// }
+			for (var x = 0; x < w2; x++) {
+				int xFloor = (int)Mathf.Floor(x * ratioX);
+				var xLerp = x * ratioX-xFloor;
+				newColors[yw + x] = ColorLerpUnclamped(ColorLerpUnclamped(texColors[y1 + xFloor], texColors[y1 + xFloor+1], xLerp),
+													   ColorLerpUnclamped(texColors[y2 + xFloor], texColors[y2 + xFloor+1], xLerp),
+													   y*ratioY-yFloor);
+			}
+		}
+	}
  
-	// private Color ColorLerpUnclamped (Color c1, Color c2, float value) {
-    //     return new Color (c1.r + (c2.r - c1.r) * value, 
-	// 					  c1.g + (c2.g - c1.g) * value, 
-	// 					  c1.b + (c2.b - c1.b) * value, 
-	// 					  c1.a + (c2.a - c1.a) * value);
-    // }
+	private Color ColorLerpUnclamped (Color c1, Color c2, float value) {
+        return new Color (c1.r + (c2.r - c1.r) * value, 
+						  c1.g + (c2.g - c1.g) * value, 
+						  c1.b + (c2.b - c1.b) * value, 
+						  c1.a + (c2.a - c1.a) * value);
+    }
 }
